7d8b19fa6e0b99a4e35a8ae8a02ffb5322e4a723
Akoji Timothy <46170255+CodeMaestro1000@users.noreply.github.com>
2020-11-26 22:11:14 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, render_template, request
+from forms import TextForm
+from summarizer import Summarizer
+# from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+# run_with_ngrok(app)
+
+@app.route("/")
+@app.route("/home")
+def home():
+	form = TextForm()
+	return render_template('index.html', form=form)
+
+@app.route("/summarize", methods=['GET', 'POST'])
+def summarize():
+	model = Summarizer()
+	if request.method == 'POST':
+		text = request.form['content']
+		result = model(body=text, ratio=0.2)
+		summary = result.split('.')
+		return render_template('summary.html', summary=summary)
+
+
+if __name__ == '__main__':
+	app.run()
